47f426da4df27984449aed592773b3fa7f2f6a1c
AliEsam99 <aesam9997@gmail.com>
2025-11-23 21:30:39 +0200

    Initial commit

---
+++ Models/Entity/PlaceVibeTag.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CairoGo.Models.Entity
+{
+
+    public class PlaceVibeTag
+    {
+        [Key]
+        public Guid PlaceVibeTagId { get; set; } = Guid.NewGuid();
+
+        [Required(ErrorMessage = "Vibe tag value is required")]
+        [StringLength(50, MinimumLength = 3)]
+        public string Value { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(Place))]
+        public Guid PlaceId { get; set; }
+
+        // Navigation Properties
+        public Place Place { get; set; } = default!;
+    }
+}
